2cfc6f3b12412ff6863074714376f189fc1bc3fa
Luciano <lopezz.lucio85@gmail.com>
2020-12-09 21:07:06 -0300

    add os models do produto

--- store/settings.py
+++ store/settings.py
@@ -10,13 +10,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'produto.apps.ProdutoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'produto.apps.ProdutoConfig',
+    
 
     #TODO: Romover debug
     'debug_toolbar',
